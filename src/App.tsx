c0edfad11dbd6d4d306846b1bf06eada6fe8ef9a
Johan Rydell <johannilsrydell89@gmail.com>
2024-04-16 15:36:03 +0200

    1-8 done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <h1>Hooks-demo</h1>
       <Counter
         initialValue={0}
-        countValue={5}
+        countValue={1}
         maxValue={5}
         minValue={-5}
         stepValue={3}
